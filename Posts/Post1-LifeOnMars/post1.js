1057a9d4061c606cd9857a1906b421c66791d04b
Dusty Shaw <shustydaw@gmail.com>
2022-05-02 17:58:45 -0600

    After updates

--- Posts/Post1-LifeOnMars/post1.js
+++ Posts/Post1-LifeOnMars/post1.js
@@ -1,17 +1,16 @@
-let locationurl = `https://api.weather.gov/points/{39.7456},-97.0892`;
-fetch(locationurl)
+let NASA_api = `https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0`;
+fetch(NASA_api)
 .then(response => response.json())
 .then (data => {
-    let hourlyForcastURL = data.properties.forecastHourly;
-    
-    fetch(hourlyForcastURL)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
+    // let MarsAverageTemp = ;
+    console.log(MarsAverageTemp);
+    // fetch(hourlyForcastURL)
+    // .then(response => response.json())
+    // .then(data => {
+    //     console.log(data);
         // let forcast = data.properties.periods[0].shortForecast;
         
         // let weather = document.getElementById("HourByHour");
         //
         // weather.innerHTML=`<p>${forcast}<p>`;
     });
-})
